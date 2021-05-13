709c835b03359e363dd5246358ce6ca4b71a3c67
Luke_Z <zurawskiu@gmail.com>
2021-05-13 23:29:33 +0200

    styling selected text enable

--- src/textEditorFontFunctions.js
+++ src/textEditorFontFunctions.js
@@ -1,11 +1,24 @@
+const styleSelectedText = (text, tag) => {
+  const selected = window.getSelection();
+  const range = selected.getRangeAt(0);
+  const textARR = [...text];
+  textARR.splice(range.startOffset, 0, `<${tag}>`);
+  textARR.splice(range.endOffset + 1, 0, `</${tag}>`);
+  if (range.startOffset === range.endOffset) {
+    document.getElementById('text').innerHTML = `<${tag}>${text}</${tag}>`;
+  } else {
+    document.getElementById('text').innerHTML = textARR.join('');
+  }
+};
+
 export const bold = () => {
   let text = document.getElementById('text').innerText;
-  document.getElementById('text').innerHTML = `<strong>${text}</strong>`;
+  styleSelectedText(text, 'strong');
 };
 
 export const italic = () => {
   let text = document.getElementById('text').innerText;
-  document.getElementById('text').innerHTML = `<i>${text}</i>`;
+  styleSelectedText(text, 'i');
 };
 
 export const addBullet = () => {
